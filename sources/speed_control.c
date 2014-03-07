7150c34e3c911eeb2d32c2f23623d42b0e8e1b49
liangzr <liangzr@outlook.com>
2014-03-07 22:54:15 +0800

    speed feedback completed
    左右轮编码器整合完成

--- sources/speed_control.c
+++ sources/speed_control.c
@@ -1,29 +1,55 @@
 #include "main.h"
+
 char MOTOR_DEAD = 0;//电机的死区占空比
 float Right_motor_speed=0,Left_motor_speed=0;//左右轮测的的速度
 float Set_right_speed=0,Set_left_speed=0;
 float Set_speed=0;
+uint32 *scount1, *scount2;		//DMA测速指定的无效传输地址
 /************************************************************/
 /* 				初始化编码器							*/
 /************************************************************/
 void Coder_init()
 {
+/*GPT测速模块*/
 	MCF_GPIO_PTAPAR|=MCF_GPIO_PTAPAR_ICOC3_ICOC3;
 	MCF_GPT_GPTPACTL=MCF_GPT_GPTPACTL_PAE		//使能脉冲累加功能
 					&~MCF_GPT_GPTPACTL_PAMOD	//累加脉冲时间模式
 					|MCF_GPT_GPTPACTL_PEDGE//累加上升沿
 					|MCF_GPT_GPTPACTL_CLK(0);    //采用GPT时时钟
 	MCF_GPT_GPTIE=0x00;//禁止GPT中断
+	
+/*DMA测速模块*/
+	MCF_DMA_DSR(1) |= MCF_DMA_DSR_DONE;					//清空传输完成标志位
+	MCF_SCM_MPR = MCF_SCM_MPR_MPR(0x05);
+	MCF_SCM_DMAREQC = MCF_SCM_DMAREQC_DMAC1(0x05);		//设定DMA1为DTIM1触发
+	MCF_DMA_SAR(1)=(uint32)scount1;			//源地址为PAN
+	MCF_DMA_DAR(1)=(uint32)scount2;				//目的地址为图像数组
+	MCF_DMA_BCR(1)=1000;									//传输长度为每行的长度，这里是320个像素
+	MCF_DMA_DCR(1)=//MCF_DMA_DCR_SSIZE_BYTE
+				  //|MCF_DMA_DCR_DSIZE_BYTE		//每次传输源地址和目的地址都为一字节
+				  //|MCF_DMA_DCR_DINC						//每次传输之后源地址保持不变，目的地址加1
+				  MCF_DMA_DCR_D_REQ						//每次传输结束之后关闭DMA外部请求
+				  |MCF_DMA_DCR_EEXT
+				  |MCF_DMA_DCR_CS;						//一次DMA请求传输一次
+	MCF_GPIO_PTCPAR |= MCF_GPIO_PTCPAR_DTIN1_DTIN1;		//设定DTIN0端口的功能
+	MCF_DTIM_DTMR(1) = 0;								//重置STMR寄存器
+	MCF_DTIM_DTMR(1) = MCF_DTIM_DTMR_CE_RISE			//捕捉上升沿
+				      |MCF_DTIM_DTMR_CLK_DTIN;				//时钟输入为DTIN
+	MCF_DTIM_DTER(1) |= MCF_DTIM_DTER_CAP;				//边沿捕捉触发
+	MCF_DTIM_DTXMR(1)|= MCF_DTIM_DTXMR_DMAEN;			//DMA请求使能
+	MCF_DTIM_DTMR(1) |= MCF_DTIM_DTMR_RST;				//开启DTIM	
+
 }
 /************************************************************/
-/* 				初始化编码器							*/
+/* 				读取速度值									*/
 /************************************************************/
 void get_speed()
 {
+	Left_motor_speed = 1000 - MCF_DMA1_BCR;
+	MCF_DMA1_BCR = 1000;
 	Right_motor_speed=MCF_GPT_GPTPACNT/0.005;//要通过测量算出一个脉冲代表的距离
 	MCF_GPT_GPTPACNT=0;
 	Car_speed=(Right_motor_speed+Left_motor_speed)/2;//两轮的速度平均值作为车向前的速度，该速度作为车速度闭环的输入量
-	
 }
 /************************************************************/
 /*                       初始化PWM模块                      */
--- sources/speed_control.h
+++ sources/speed_control.h
@@ -1,3 +1,7 @@
+#define SPEED_DMA 10000 - MCF_DMA1_BCR	//DMA≤‚ÀŸ÷µ
+
+extern float Right_motor_speed, Left_motor_speed;
+
 void PWM_INIT(void);
 void set_motor_highduty(char highdutyA,char highdutyB);
 void Coder_init();
